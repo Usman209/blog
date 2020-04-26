b20a33cd955c7a993e754d91c8129a331d013151
Unknown <usmanjamil196@gmail.com>
2020-04-26 07:25:24 +0500

    blog front set up

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+<add-blog></add-blog>
+</div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import addBlog from './components/addBlog'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    'add-blog':addBlog
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
